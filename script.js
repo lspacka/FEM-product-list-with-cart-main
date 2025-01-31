0f85b00ed6b82c5fbd41db36396bbb8fab66a98c
julian <jdrc@protonmail.com>
2025-01-30 20:13:57 -0500

    update

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const desserts_data = document.querySelectorAll('.dessert-info')
 const buttons = document.querySelectorAll('.add-button')
+const cart_items = document.querySelector('.cart-items')
+const cart_content = document.querySelector('.cart-content')
 const desserts = []
 const cart = []
-const order_btns = []
-let desserts_quant = 0
+// const order_btns = []
+let order_quant = 0
 let total = 0
 
 class Dessert {
@@ -21,20 +23,21 @@ for (let data of desserts_data) {
 
     dessert.type = array[0].textContent
     dessert.name = array[1].textContent
-    dessert.price = array[2].textContent
+    // dessert.price = Number(array[2].textContent.match(/[\d.]+/)[0])
+    dessert.price = array[2].textContent.split('$')[1]
     dessert.quantity = 0
     desserts.push(dessert)
 }
 
+cart_items.textContent = `(${order_quant})`
+
 buttons.forEach((button, index) => {
     button.addEventListener('click', () => {
-        console.log(index)
+        // console.log(index)
         if (!cart.includes(desserts[index])) {
             cart.push(desserts[index])
             desserts[index].quantity++
             // console.log(cart)
-            // this just for testing
-            // better to have the buttons already created and styled, and then just add them here
             button.innerHTML = `
                 <div class="dec-quant" id="dec-${index}">
                     <img src="assets/images/icon-decrement-quantity.svg" alt="decrement sign" class="order-img" >
@@ -44,64 +47,62 @@ buttons.forEach((button, index) => {
                     <img src="assets/images/icon-increment-quantity.svg" alt="increment sign" class="order-img">
                 </div>
             `
+
             button.style.backgroundColor = 'hsl(14, 86%, 42%)'
             button.style.border = 'none'
             button.style.justifyContent = 'space-evenly'
             button.style.alignItems = 'center'
             button.style.cursor = 'initial'
+            // order_btns.push(button)
 
-            order_btns.push(button)
+            order_quant++
+            cart_items.textContent = `(${order_quant})`
 
             const dec_btn = document.getElementById(`dec-${index}`)
             const inc_btn = document.getElementById(`inc-${index}`)
             const order_label = document.getElementById(`label-${index}`)
 
-            // order_label.textContent = `${desserts[index].quantity}`
-
             dec_btn.addEventListener('click', () => {
                 if (desserts[index].quantity == 0)
-                    return  // change to normal button?
+                    return          // change to normal button?
+                                              
                 desserts[index].quantity--
+                order_quant--
                 order_label.textContent = `${desserts[index].quantity}`
+                cart_items.textContent = `(${order_quant})`
+
+                // console.log(cart)
+                // console.log('order quantity: ', order_quant)
+                // console.log(typeof desserts[index].price)
                 // console.log(desserts[index].quantity)
                 // console.log(desserts[index])
             })
 
             inc_btn.addEventListener('click', () => {
                 desserts[index].quantity++
+                order_quant++
                 order_label.textContent = `${desserts[index].quantity}`
+                cart_items.textContent = `(${order_quant})`
+                // console.log(cart)
+                // console.log('order quantity', order_quant)
                 // console.log(desserts[index].quantity)
                 // console.log(desserts[index])
             })
 
+            // ul.item-list
+            // carbon free message
+            // confirm order button and event listener
             
+            // cart.forEach((item, index) => {
+            //   create elements with classes
+            //   append elements to parent <li> 
+            // })
+
             // button.style.padding = '0.7em 0 0.7em 0'
             // change cart
         } else {
             return
         }
-
-        // order_btns.forEach((button, index) => {
-        //     const dec_btn = document.querySelector('.dec-quant')
-        //     const inc_btn = document.querySelector('.inc-quant')
-        //     const order_label = document.querySelector('.order-label')
-
-        //     dec_btn.addEventListener('click', () => {
-        //         if (desserts[index].quantity == 0)
-        //             return  // change to normal button?
-        //         cart[index].quantity--
-        //         order_label.textContent = `${cart[index].quantity}`
-        //         // console.log(desserts[index].quantity)
-        //         console.log(cart[index])
-        //     })
-
-        //     inc_btn.addEventListener('click', () => {
-        //         cart[index].quantity++
-        //         order_label.textContent = `${cart[index].quantity}`
-        //         // console.log(desserts[index].quantity)
-        //         console.log(desserts[index])
-        //     })
-        // })
 
         // desserts[index].quantity++
         // console.log(cart)
